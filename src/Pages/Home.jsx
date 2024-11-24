9b665d11eb14cc3b1e7fbe3925a963e0a17d136d
melika <fatemeh.najafpour2018@gmail.com>
2024-11-24 14:06:13 +0330

    add base project and navbar

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <>
+      <div
+        className="relative h-screen bg-cover bg-center"
+        style={{ backgroundImage: "url('/Images/4.jpg')" }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
+          <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
+            Explore the world with Us
+          </h1>
+          <p className="text-lg md:text-2xl text-white mb-8">
+            Discover amazing places at exclusive deals
+          </p>
+          <button className="border text-white px-6 py-2 rounded-full text-lg md:text-xl hover:bg-blue-400 transform transition duration-300 hover:scale-105">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
